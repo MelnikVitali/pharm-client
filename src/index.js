34da293871fec8fac843cb67bc6f8eed7959d54a
MelnikVitali <mvitalim@gmail.com>
2020-06-11 23:59:49 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from "redux-thunk";
+import logger from 'redux-logger';
+import { BrowserRouter } from 'react-router-dom';
+
+import rootReducer from './store/reducers';
+
+import App from './components/App';
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
 );
 
